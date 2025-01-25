a7f8cb78006407212d056e66da1f011d215929b1
Gwen Tan <gw3nnipi3@gmail.com>
2025-01-25 16:10:30 +0000

    Add files via upload

---
+++ people.js
@@ -0,0 +1,13 @@
+var women = [
+  {name: "Rep. Tulsi Gabbard", title: "Congresswoman", photo: "http://media.washtimes.com.s3.amazonaws.com/media/image/2015/10/13/AP_104281239272.jpg"},
+  {name: "Justice Ruth Bader Ginsburg", title: "Supreme Court Justice", photo: "http://i2.cdn.cnn.com/cnnnext/dam/assets/111102115632-justice-ruth-bader-ginsburg-story-top.jpg"},
+  {name: "Sen. Kirsten Gillibrand", title: "U.S Senator", photo: "https://s-media-cache-ak0.pinimg.com/originals/db/d4/c5/dbd4c5a06b9f69755f53cce2ae9d7a05.jpg"},
+  // ...existing code...
+];
+
+var men = [
+  {name: "Speaker Paul Ryan", title: "Speaker of the House", photo: "http://static2.politico.com/dims4/default/273ab82/2147483647/resize/1160x%3E/quality/90/?url=http%3A%2F%2Fstatic.politico.com%2Fb8%2F1d%2F49ba6d4445a5a190717f236d0a10%2F151210-paul-ryan-1160-gty.jpg"},
+  {name: "Chuck Todd", title: "Host, Meet the Press", photo: "http://a4.img.talkingpointsmemo.com/image/upload/c_fill,fl_keep_iptc,g_faces,h_365,w_652/drrgkjnfpvhqj3w3rrds.jpg"},
+  {name: "Sen. Martin Heinrich", title: "U.S Senator", photo: "http://greginhollywood.com/wordpress/wp-content/uploads/121107053753-martin-heinrich-story-top.jpg"},
+  // ...existing code...
+];
